dff0cbf6de4b20c71b6d41cc46f3716e7ca2e344
sho <sskmy1024.y@gmail.com>
2019-04-09 14:22:25 +0900

    keyword added

--- database/seeds/GameThemekeyordSeeder.php
+++ database/seeds/GameThemekeyordSeeder.php
@@ -30,7 +30,30 @@ class GameThemeKeywordSeeder extends Seeder
             "word" => "天才",
         ]);
         DB::table("game_theme_keywords")->insert([
-            "word" => "工科大",
+            "word" => "マジ",
+        ]);
+
+
+        DB::table("game_theme_keywords")->insert([
+            "word" => "まあね",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "東京タワー",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "チェケラ",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "LINE",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "Twitter",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "多分",
+        ]);
+        DB::table("game_theme_keywords")->insert([
+            "word" => "ゲーム",
         ]);
     }
 }
